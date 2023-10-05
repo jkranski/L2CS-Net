a87965aba413f45db73c9c8e2ce8b90ba31e20ef
Josh Faust <joshf@ust.me>
2023-10-04 20:16:41 -0700

    Use control change messages, and send out all channels based on the gaze inputs

--- game.py
+++ game.py
@@ -4,15 +4,17 @@ import mido
 
 notes = [20, 40, 60, 80]
 
+
 async def main():
     midi_port = mido.open_output(mido.get_output_names()[0])
     receiver = GazeReceiver()
     while True:
         data = await receiver.recv()
-        index = data.column_counts.index(max(data.column_counts))
-        print(index)
-        msg = mido.Message('note_on', note=notes[index], channel=index)
-        midi_port.send(msg)
-    
+        for i in range(0, 4):
+            msg = mido.Message(
+                'control_change', value=data.column_counts[i], channel=i)
+            print(msg)
+            midi_port.send(msg)
+
 if __name__ == '__main__':
     asyncio.run(main())
